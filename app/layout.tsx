6bb36d8531f5c7dec0003187476ab9f1996b4beb
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-07-29 12:55:08 -0500

    remove google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import Topbar from './components/topbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'RSC | AAD | Prisma',
