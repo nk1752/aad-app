4c04ccd9df6b714d05cf7e60ddb01a5a616329d6
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-08-14 18:50:27 -0500

    no metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const msalConfig: Configuration = {
   auth: {
     clientId: 'ddec4f98-52aa-40b2-801e-31531e319288',
     authority: 'https://login.microsoftonline.com/7cb752a7-6dfd-429e-adc9-129f0ea3fcec',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: '/',
   },
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className=' flex flex-row text-stone-100 content-center items-center justify-center'>
       <div className=" bg-gray-700 text-amber-500 w-50 min-h-[10%] p-8 rounded-lg shadow-xl">
-        <h1>Please Sign In</h1>
+        <h1>Home Page</h1>
       </div>
     </main>
   );
