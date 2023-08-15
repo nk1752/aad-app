986d445ce511da408d0f476fb128c0a33c85130f
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-08-14 20:37:05 -0500

    no .env for Prisma

--- app/fetchdata/page.tsx
+++ app/fetchdata/page.tsx
@@ -5,7 +5,8 @@ export default function Page() {
     return (
         <main className=' flex flex-row text-stone-100 content-center items-center justify-center'>
             <div className=" bg-gray-700 text-amber-500 w-50 min-h-[10%] p-8 rounded-lg shadow-xl">
-                <h1>Weather Home Page</h1>
+                <h1>Weather Home Page</h1><br />
+                <p>{process.env.DATABASE_URL}</p>
             </div>
         </main>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ const metadata: Metadata = {
   description: 'RSC, Prisma ORM, and AAD',
 }
 
-
 // MSAL configuration
 const msalConfig: Configuration = {
   auth: {
