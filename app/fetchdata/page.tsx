08a5549612dafd8d8dd14e781df1ec83c52e7672
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-08-15 13:37:20 -0500

    use env

--- app/fetchdata/page.tsx
+++ app/fetchdata/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         <main className=' flex flex-row text-stone-100 content-center items-center justify-center'>
             <div className=" bg-gray-700 text-amber-500 w-50 min-h-[10%] p-8 rounded-lg shadow-xl">
                 <h1>Weather Home Page</h1><br />
-                <p>{process.env.DATABASE_URL}</p>
+                <p>URL: {process.env.DATABASE_URL}</p>
             </div>
         </main>
     );
