f0de9cda9144368fd031da49e15628f6b5035697
nk1752 <71557302+nk1752@users.noreply.github.com>
2023-11-29 08:55:14 -0600

    remove url from fetchdata

--- app/fetchdata/page.tsx
+++ app/fetchdata/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
     return (
         <main className=' flex flex-row text-stone-100 content-center items-center justify-center'>
             <div className=" bg-gray-700 text-amber-500 w-50 min-h-[10%] p-8 rounded-lg shadow-xl">
-                <h1>Weather Home Page</h1><br />
-                <p>URL: {process.env.DATABASE_URL}</p>
+                <h1>Data Home Page</h1><br />
+                
             </div>
         </main>
     );
